e333e4d2f71a729ceadfc15c26f0e031824b41af
Marco Yuen <marcoyuen2006@gmail.com>
2020-10-28 15:40:34 +0800

    first commit

--- hotzone_config/hotzone_config/settings.py
+++ hotzone_config/hotzone_config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
