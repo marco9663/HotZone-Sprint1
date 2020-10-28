ac45390a309debae57a70b3abb56c0753ff23416
Marco Yuen <marcoyuen2006@gmail.com>
2020-10-28 16:34:10 +0800

    first commit

--- hotzone_config/hotzone_config/settings.py
+++ hotzone_config/hotzone_config/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'hotzone_config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
 
 # Password validation
